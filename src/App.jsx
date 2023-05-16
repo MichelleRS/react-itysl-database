2e0e3787a1f93a81162669af8fd9042f7493c2dc
Michelle Ross <miross629@gmail.com>
2023-05-16 10:44:58 -0700

    Display sketches as list

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+/* imports */
+import { useState } from "react";
+import sketchData from "./sketch-data.js";
+
 export default function App() {
+  console.log("sketchData", sketchData);
+  const [filteredSketches] = useState(sketchData);
+
   return (
     <>
       <h1>I Think You Should Leave Database</h1>
+      <ul>
+        {filteredSketches.map((sketch) => {
+          const { id, season, episode, title } = sketch;
+          return (
+            <li key={id}>
+              <h3>{title}</h3>
+              <p>
+                Season {season}, Episode {episode}
+              </p>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
