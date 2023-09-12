b4a07243d909bc51828a93aac30a2cbe098b6e21
Michelle Ross <miross629@gmail.com>
2023-09-12 11:16:38 -0700

    Fix filter by season, TODO toggle IncludesTim

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import sketchData from "./sketch-data.js";
 export default function App() {
   const sketches = sketchData;
   const [filteredSketches, setFilteredSketches] = useState(sketchData);
+  console.log("filteredSketches", filteredSketches);
   const [season, setSeason] = useState();
+  const [includesTim, setIncludesTim] = useState(false);
   // initialize variable for seasons
   // use set to filter by unique values
   const seasons = Array.from(new Set(sketches.map((sketch) => sketch.season)));
@@ -22,14 +24,32 @@ export default function App() {
     setFilteredSketches(filteredBySeason);
   }
 
+  // TODO - toggle display of sketches
+  function toggleIncludesTim() {
+    // update state for includesTim
+    setIncludesTim(!includesTim);
+    // initialize variable for sketches that include tim
+    const filteredByTim = sketches.filter((sketch) => {
+      // check for sketches that include tim
+      if (sketch.includesTim === true) {
+        // add sketch to new array
+        return { ...sketch };
+      }
+    });
+    console.log("filteredByTim", filteredByTim);
+    setFilteredSketches(filteredByTim);
+    console.log("filteredSketches in toggle", filteredSketches);
+  }
+
   return (
     <>
       <h1>I Think You Should Leave Database</h1>
       {/* select season */}
+      {/* TODO: fix console error: Incorrect use of <label for=FORM_ELEMENT> */}
       <label htmlFor="season">Select Season</label>
       <select
-        name="select"
-        id={season}
+        name="season"
+        id="season"
         onChange={(e) => filterBySeason(e.target.value)}
       >
         <option value="">All</option>
@@ -41,7 +61,16 @@ export default function App() {
           );
         })}
       </select>
-
+      {/* sketches with only tim */}
+      <input
+        type="checkbox"
+        id="includesTim"
+        name="includesTim"
+        defaultChecked={false}
+        // checked={includesTim}
+        onChange={(e) => toggleIncludesTim(e.target.value)}
+      />
+      <label htmlFor="includesTim">Only show sketches with Tim Robinson</label>
       {/* display sketches */}
       <ul>
         {filteredSketches.map((sketch) => (
