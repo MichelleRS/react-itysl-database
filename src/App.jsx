30c6a2119304578098804ac5aecde3126aea7eb6
Michelle Ross <miross629@gmail.com>
2023-09-14 14:41:43 -0700

    Refactor filtering as form with checkboxes

--- src/App.jsx
+++ src/App.jsx
@@ -7,85 +7,52 @@ export default function App() {
   // initialize variables
   // get sketches object
   const sketches = sketchData;
-  // get seasons for select element; use set to filter by unique values
-  const seasons = Array.from(new Set(sketches.map((sketch) => sketch.season)));
+  // initialize variable to hold array of filter labels
+  const filterCategories = ["Season 1", "Season 2", "Season 3", "Includes Tim"];
 
   // state
   const [filteredSketches, setFilteredSketches] = useState(sketchData);
-  // TODO: how is this being used?
-  const [season, setSeason] = useState("");
-  const [includesTim, setIncludesTim] = useState(false);
-  // TODO: sync with checkbox toggle
-  function filterBySeason(season) {
-    // update state for season
-    setSeason(season);
 
-    // if "all" selected:
-    if (season === "all") {
-      setFilteredSketches(sketches);
-    }
-    // if a season is selected
-    else {
-      // initialize variable to hold sketches in selected season
-      const filteredBySeason = sketches.filter(
-        (sketch) => sketch.season === Number(season)
-      );
-      // update filtered sketches with selected season
-      setFilteredSketches(filteredBySeason);
-    }
+  function toggleFilter(e) {
+    console.log("I clicked on a checkbox!");
+    console.log("Am I checked?", e);
   }
-
-  // TODO: toggle display of sketches
-  function toggleIncludesTim() {
-    // update state for includesTim
-    setIncludesTim(!includesTim);
-    // initialize variable for sketches that include tim
-    const filteredByTim = sketches.filter((sketch) => {
-      // check for sketches that include tim
-      if (sketch.includesTim === true) {
-        // add sketch to new array
-        return { ...sketch };
-      }
-    });
-    setFilteredSketches(filteredByTim);
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("I applied the filters!");
+    console.log("e", e);
+    // TODO check filter categories selected
+    // TODO return sketches with those filter categories
   }
-
   function resetFilters() {
+    console.log("I clicked reset button!");
     setFilteredSketches(sketches);
-    // TODO: set select to "All"
+    // TODO uncheck inputs
   }
 
   return (
     <>
       <h1>I Think You Should Leave Database</h1>
-      {/* select: season */}
-      <label htmlFor="season">Select Season</label>
-      <select
-        name="season"
-        id="season"
-        onChange={(e) => filterBySeason(e.target.value)}
-      >
-        <option value="all">All</option>
-        {seasons.map((season) => {
+      <form onSubmit={handleSubmit}>
+        {filterCategories.map((category) => {
           return (
-            <option key={season} value={season}>
-              Season {season}
-            </option>
+            <>
+              <input
+                type="checkbox"
+                id={category}
+                key={category}
+                name={category}
+                onChange={(e) => toggleFilter(e.target.checked)}
+              />
+              <label htmlFor={category}>{category}</label>
+            </>
           );
         })}
-      </select>
-      {/* checkbox: toggle sketches with tim */}
-      <input
-        type="checkbox"
-        id="includesTim"
-        name="includesTim"
-        onChange={(e) => toggleIncludesTim(e.target.value)}
-      />
-      <label htmlFor="includesTim">Only show sketches with Tim Robinson</label>
-      {/* TODO: button to reset filters */}
-      <button type="button" onClick={resetFilters}>
-        Reset
-      </button>
+        <button type="submit">Apply Filters</button>
+        <button type="button" onClick={resetFilters}>
+          Reset Filters
+        </button>
+      </form>
       {/* display sketches */}
       <ul>
         {filteredSketches.map((sketch) => (
